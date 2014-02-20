cfcd6a9122bc94c0c7a26a2442423364d866341c
Alumno <alumno@localhost.localdomain>
2014-02-20 10:44:49 -0600

    avance3

--- Examen_Marcos/src/com/me/mygdxgame/MyGdxGame.java
+++ Examen_Marcos/src/com/me/mygdxgame/MyGdxGame.java
@@ -15,12 +15,19 @@ public class MyGdxGame implements ApplicationListener {
 	private SpriteBatch batch; 
 	private Texture texture; 
 	private Sprite sprite; 
-	private Texture texture_nave; 
-	private Sprite sprite_nave; 
-	 
-	 
-	int Rotacion=0; 
-	 
+	private Texture texture_enemigo; 
+	private Sprite sprite_enemigo; 
+	private Texture texture_fondo;
+	private Sprite sprite_fondo;
+	private Texture texture_nave;
+	private Sprite sprite_nave;
+	
+	int rotacion=0;
+	float translate;
+	
+
+	
+	
 	@Override 
 	public void create() {		 
 		float w = Gdx.graphics.getWidth(); 
@@ -31,7 +38,10 @@ public class MyGdxGame implements ApplicationListener {
 		 
 		texture = new Texture(Gdx.files.internal("data/libgdx.png")); 
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear); 
-		texture_nave = new Texture(Gdx.files.internal("data/estrella.png")); 
+		texture_enemigo = new Texture(Gdx.files.internal("data/enemigo.png")); 
+		texture_fondo = new Texture(Gdx.files.internal("data/fondo.png")); 
+		texture_nave = new Texture(Gdx.files.internal("data/nave.png")); 
+		
 		 
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275); 
 		 
@@ -41,10 +51,20 @@ public class MyGdxGame implements ApplicationListener {
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2); 
 		sprite.setPosition(0,0); 
 		 
-		sprite_nave = new Sprite(texture_nave,201,101); 
-		sprite_nave.setPosition(-0.2f,-0.2f); 
-		sprite_nave.setSize(0.2f,0.2f); 
+		sprite_enemigo = new Sprite(texture_enemigo,201,101); 
+		sprite_enemigo.setPosition(0.9f,-0.1f); 
+		sprite_enemigo.setSize(0.2f,0.2f);
+		sprite_enemigo.setOrigin(sprite_enemigo.getWidth()/2, sprite_enemigo.getHeight()/2); 
+		
+		sprite_enemigo.setRotation(12 );
 		 
+		sprite_fondo = new Sprite(texture_fondo,512,512); 
+		sprite_fondo.setPosition(-0.5f,-0.4f); 
+		sprite_fondo.setSize(1f,1f);
+		
+		sprite_nave = new Sprite(texture_nave,128,64); 
+		sprite_nave.setPosition(-0.5f,-0.1f); 
+		sprite_nave.setSize(0.2f,0.2f);
 		 
 	} 
  
@@ -55,24 +75,33 @@ public class MyGdxGame implements ApplicationListener {
 	} 
 
 	@Override 
-	public void render() {		 
+	public void render() {	
+		
+		
 		Gdx.gl.glClearColor(0.4f, 0.5f,1 ,1 ); 
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); 
-		sprite.setRotation(Rotacion); 
-		Rotacion++; 
-		 
-		 
-		 
-		 
+		
 		 
 		batch.setProjectionMatrix(camera.combined); 
-		batch.begin(); 
-		sprite_nave.draw(batch); 
-		sprite.draw(batch); 
+		batch.begin();
+		sprite_fondo.draw(batch);
+		sprite_nave.draw(batch);
+		sprite_enemigo.draw(batch);
+		
+		//sprite.draw(batch);
 		batch.end(); 
+		
+		sprite_enemigo.setX(translate);
+		
+		rotacion++;
+		sprite_enemigo.setRotation(rotacion);
+		if(Gdx.input.isTouched()){
+			translate-=0.01;
+			rotacion+=20;
+			
+		}
 		 
-		 
-	} 
+	}
 
 	@Override 
 	public void resize(int width, int height) { 
